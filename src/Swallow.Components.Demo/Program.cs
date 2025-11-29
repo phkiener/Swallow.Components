2512e889ebd8b7a6664faddcca4209e623c3740f
Philipp Kiener <kiener.phil@gmail.com>
2025-11-29 22:07:39 +0100

    Define rough grid for demo page

--- src/Swallow.Components.Demo/Program.cs
+++ src/Swallow.Components.Demo/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Swallow.Components.Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("body::after");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 await builder.Build().RunAsync();
