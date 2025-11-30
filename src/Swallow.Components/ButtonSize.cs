37b87eeae06c550fb84601f44d0856176f1c72b9
Philipp Kiener <kiener.phil@gmail.com>
2025-11-30 01:09:13 +0100

    Start dogfooding; use SwButton for theme switch

---
+++ src/Swallow.Components/ButtonSize.cs
@@ -0,0 +1,10 @@
+namespace Swallow.Components;
+
+public enum ButtonSize
+{
+    Small = 100,
+    Smaller = 300,
+    Normal = 500,
+    Larger = 700,
+    Large = 900
+}
--- src/Swallow.Components/SwButton.razor.cs
+++ src/Swallow.Components/SwButton.razor.cs
@@ -6,6 +6,10 @@ namespace Swallow.Components;
 public sealed partial class SwButton : ComponentBase
 {
     private string buttonType => OnClick.HasDelegate ? "button" : "submit";
+    private string buttonClass => string.Join(" ", EnumerateClasses());
+
+    [Parameter]
+    public ButtonSize Size { get; set; } = ButtonSize.Normal;
 
     [Parameter]
     [EditorRequired]
@@ -16,4 +20,22 @@ public sealed partial class SwButton : ComponentBase
 
     [Parameter(CaptureUnmatchedValues = true)]
     public IDictionary<string, object?>? AdditionalAttributes { get; set; }
+
+    private IEnumerable<string?> EnumerateClasses()
+    {
+        yield return Size switch
+        {
+            ButtonSize.Small => "size-small",
+            ButtonSize.Smaller => "size-smaller",
+            ButtonSize.Normal => "size-normal",
+            ButtonSize.Larger => "size-larger",
+            ButtonSize.Large => "size-large",
+            _ => null
+        };
+
+        if (AdditionalAttributes?.TryGetValue("class", out var extraClass) ?? false)
+        {
+            yield return extraClass?.ToString();
+        }
+    }
 }
