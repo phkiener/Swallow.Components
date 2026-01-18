b9936e140eb39c66d85c37f92390e0063c11242b
Philipp Kiener <kiener.phil@gmail.com>
2026-01-18 20:09:25 +0100

    Turn SwInputText into SwPlainInputText and introduce a fancier SwInputText

--- src/components/Swallow.Components/Forms/SwInputText.razor.cs
+++ src/components/Swallow.Components/Forms/SwInputText.razor.cs
@@ -1,13 +1,32 @@
 using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 
 namespace Swallow.Components.Forms;
 
 /// <summary>
-/// A basic text input, similar to <see cref="InputText"/>.
+/// A text input supporting a handful of decorations.
 /// </summary>
 public partial class SwInputText : InputBase<string?>
 {
+    /// <summary>
+    /// A prefix to render before the actual input.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? Prefix { get; set; }
+
+    /// <summary>
+    /// A suffx to render after the actual input.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? Suffix { get; set; }
+
+    /// <summary>
+    /// An action, most likely a button, to render after the suffix.
+    /// </summary>
+    [Parameter]
+    public RenderFragment? Action { get; set; }
+
     /// <inheritdoc />
     protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
     {
---
+++ src/components/Swallow.Components/Forms/SwPlainInputText.razor.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace Swallow.Components.Forms;
+
+/// <summary>
+/// A basic text input, similar to <see cref="InputText"/>.
+/// </summary>
+public partial class SwPlainInputText : InputBase<string?>
+{
+    /// <inheritdoc />
+    protected override bool TryParseValueFromString(string? value, out string? result, [NotNullWhen(false)] out string? validationErrorMessage)
+    {
+        result = value;
+        validationErrorMessage = null;
+
+        return true;
+    }
+}
