ed568f7badbb33d4d9b965857a4d01220f6f5a33
Philipp Kiener <kiener.phil@gmail.com>
2026-05-03 14:24:59 +0200

    Refactor..

--- test/reactive/Swallow.Components.Reactive.Test/Rendering/EventHandlers/HandlerRegistrationTest.razor.cs
+++ test/reactive/Swallow.Components.Reactive.Test/Rendering/EventHandlers/HandlerRegistrationTest.razor.cs
@@ -1,10 +1,4 @@
 using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using NUnit.Framework;
 using Swallow.Components.Reactive.Rendering.EventHandlers;
 
@@ -22,29 +16,9 @@ public sealed partial class HandlerRegistrationTest
 
     private async Task DiscoverEventHandlersAsync(RenderFragment renderFragment)
     {
-        var serviceCollection = new ServiceCollection();
-        serviceCollection.AddRazorComponents();
-
-        await using var serviceProvider = serviceCollection
-            .AddSingleton<ILoggerFactory, NullLoggerFactory>()
-            .AddSingleton(typeof(ILogger<>), typeof(NullLogger<>))
-            .AddSingleton<IConfiguration>(new ConfigurationRoot([]))
-            .AddSingleton<IWebHostEnvironment, FakeWebHostEnvironment>()
-            .BuildServiceProvider();
-
-        var renderer = new TestableRenderer(serviceProvider, NullLoggerFactory.Instance);
+        await using var renderer = TestableRenderer.Create();
         await renderer.Render(renderFragment);
 
         handlerRegistration.DiscoverEventDescriptors(0, renderer.GetFrames);
-    }
-
-    private sealed class FakeWebHostEnvironment : IWebHostEnvironment
-    {
-        public string ApplicationName { get; set; } = "";
-        public IFileProvider ContentRootFileProvider { get; set; } = new NullFileProvider();
-        public string ContentRootPath { get; set; } = "";
-        public string EnvironmentName { get; set; } = "";
-        public string WebRootPath { get; set; } = "";
-        public IFileProvider WebRootFileProvider { get; set; } =  new NullFileProvider();
     }
 }
--- test/reactive/Swallow.Components.Reactive.Test/TestableRenderer.cs
+++ test/reactive/Swallow.Components.Reactive.Test/TestableRenderer.cs
@@ -2,12 +2,24 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.HtmlRendering.Infrastructure;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Swallow.Components.Reactive.Test;
 
-public sealed class TestableRenderer(IServiceProvider serviceProvider, ILoggerFactory loggerFactory) : StaticHtmlRenderer(serviceProvider, loggerFactory)
+public sealed class TestableRenderer : StaticHtmlRenderer
 {
+    private readonly IServiceProvider serviceProvider;
+
+    private TestableRenderer(IServiceProvider serviceProvider) : base(serviceProvider, NullLoggerFactory.Instance)
+    {
+        this.serviceProvider = serviceProvider;
+    }
+
     public Task Render(RenderFragment renderFragment)
     {
         var hostParameters = new Dictionary<string, object?> { [nameof(ContentHost.Body)] = renderFragment };
@@ -17,6 +29,29 @@ public sealed class TestableRenderer(IServiceProvider serviceProvider, ILoggerFa
             var root = BeginRenderingComponent(typeof(ContentHost), ParameterView.FromDictionary(hostParameters));
             await root.QuiescenceTask;
         });
+    }
+
+    public static TestableRenderer Create(Action<IServiceCollection>? configure = null)
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddRazorComponents();
+        serviceCollection.AddSingleton<ILoggerFactory, NullLoggerFactory>();
+        serviceCollection.AddSingleton(typeof(ILogger<>), typeof(NullLogger<>));
+        serviceCollection.AddSingleton<IConfiguration>(new ConfigurationRoot([]));
+        serviceCollection.AddSingleton<IWebHostEnvironment, FakeWebHostEnvironment>();
+        configure?.Invoke(serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        return new TestableRenderer(serviceProvider);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (serviceProvider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
     }
 
     public ArrayRange<RenderTreeFrame> GetFrames(int componentId) => GetCurrentRenderTreeFrames(componentId);
@@ -31,5 +66,15 @@ public sealed class TestableRenderer(IServiceProvider serviceProvider, ILoggerFa
         {
             builder.AddContent(0, Body);
         }
+    }
+
+    private sealed class FakeWebHostEnvironment : IWebHostEnvironment
+    {
+        public string ApplicationName { get; set; } = "";
+        public IFileProvider ContentRootFileProvider { get; set; } = new NullFileProvider();
+        public string ContentRootPath { get; set; } = "";
+        public string EnvironmentName { get; set; } = "";
+        public string WebRootPath { get; set; } = "";
+        public IFileProvider WebRootFileProvider { get; set; } =  new NullFileProvider();
     }
 }
