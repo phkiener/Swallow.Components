0bb78dd5450236b243548bf8c0b303bfd6ab9787
Philipp Kiener <kiener.phil@gmail.com>
2026-05-31 22:15:41 +0200

    Button Group

---
+++ src/components/Swallow.Components/Actions/SwButtonGroup.razor.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Components;
+using Swallow.Components.Features;
+
+namespace Swallow.Components.Actions;
+
+/// <summary>
+/// A way of coupling multiple <see cref="SwButton"/>s together.
+/// </summary>
+public sealed partial class SwButtonGroup : ComponentBase, IHandleAttributes
+{
+    /// <summary>
+    /// Whether the buttons should be joined together or keep a certain distance.
+    /// </summary>
+    [Parameter]
+    public bool Joined { get; set; }
+
+    /// <summary>
+    /// The content to display.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public required RenderFragment ChildContent { get; set; }
+
+    /// <inheritdoc />
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object?>? AdditionalAttributes { get; set; }
+}
