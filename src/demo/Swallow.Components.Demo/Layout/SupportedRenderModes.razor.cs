48955e14ed6981604828c1b180c19258a417bc0d
Philipp Kiener <kiener.phil@gmail.com>
2026-06-02 18:46:53 +0200

    Icon with overridable sizes

---
+++ src/demo/Swallow.Components.Demo/Layout/SupportedRenderModes.razor.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Swallow.Components.Demo.Layout;
+
+public sealed partial class SupportedRenderModes : ComponentBase
+{
+    [Parameter]
+    public bool Static { get; set; }
+
+    [Parameter]
+    public bool WebSocket { get; set; }
+
+    [Parameter]
+    public bool WebAssembly { get; set; }
+}
