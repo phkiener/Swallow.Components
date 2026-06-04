a5467e0475c349c553fa7f00b48ff948b51f0038
Philipp Kiener <kiener.phil@gmail.com>
2026-06-04 17:47:31 +0200

    Alright, use embedding as well

--- src/demo/Swallow.Components.Demo/Layout/ComponentExample.razor.cs
+++ src/demo/Swallow.Components.Demo/Layout/ComponentExample.razor.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Components;
 using Swallow.Components.Demo.Utils;
 
@@ -5,16 +6,63 @@ namespace Swallow.Components.Demo.Layout;
 
 public sealed partial class ComponentExample(RazorRenderer razorRenderer, MarkupRenderer markupRenderer) : ComponentBase
 {
-    private string renderedMarkup = "";
-    private string renderedRazor = "";
+    private RenderFragment? renderedComponent;
+    private string? renderedRazor;
+    private string? renderedMarkup;
 
     [Parameter]
-    [EditorRequired]
-    public required RenderFragment ChildContent { get; set; }
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public Type? ExampleComponent { get; set; }
 
     protected override async Task OnParametersSetAsync()
     {
-        renderedRazor = await razorRenderer.RenderAsync(ChildContent);
-        renderedMarkup = await markupRenderer.RenderAsync(ChildContent);
+        if (ChildContent is null && ExampleComponent is null)
+        {
+            throw new ArgumentException($"At least one of either {nameof(ChildContent)} or {nameof(ExampleComponent)} must be specified.");
+        }
+
+        if (ChildContent is not null && ExampleComponent is not null)
+        {
+            throw new ArgumentException($"You cannot specify both {nameof(ChildContent)} and {nameof(ExampleComponent)}.");
+        }
+
+        if (ChildContent is not null)
+        {
+            renderedComponent = ChildContent;
+            renderedRazor = await razorRenderer.RenderAsync(ChildContent);
+            renderedMarkup = await markupRenderer.RenderAsync(ChildContent);
+        }
+
+        if (ExampleComponent is not null)
+        {
+            renderedComponent = RenderComponent(ExampleComponent);
+
+            renderedRazor = await LoadEmbeddedFileAsync(ExampleComponent.Assembly, ExampleComponent.FullName + ".razor");
+            renderedMarkup = await markupRenderer.RenderAsync(renderedComponent);
+        }
+    }
+
+    private static async Task<string?> LoadEmbeddedFileAsync(Assembly assembly, string name)
+    {
+        await using var manifestStream = assembly.GetManifestResourceStream(name);
+        if (manifestStream is null)
+        {
+            return null;
+        }
+
+        using var reader = new StreamReader(manifestStream);
+        return await reader.ReadToEndAsync();
+    }
+
+    private static RenderFragment RenderComponent(Type type)
+    {
+        return builder =>
+        {
+            builder.OpenComponent<DynamicComponent>(0);
+            builder.AddAttribute(1, nameof(DynamicComponent.Type), type);
+            builder.CloseComponent();
+        };
     }
 }
